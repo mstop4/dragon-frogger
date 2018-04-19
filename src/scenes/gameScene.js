63909755035c887b0b1b4d3652ae2f3ecb54ca9e
mstop4 <mstop4@gmail.com>
2018-04-19 14:38:27 -0400

    added player

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -5,14 +5,22 @@ export class GameScene extends Scene {
     super({
       key: 'gameScene'
     })
+
+    this.player = null
   }
 
   preload() {
     this.load.image('background', 'assets/background.png')
+    this.load.image('player', 'assets/player.png')
+    this.load.image('dragon', 'assets/dragon.png')
+    this.load.image('treasure', 'assets/treasure.png')
   }
 
   create() {
     let bg = this.add.sprite(0, 0, 'background')
     bg.setOrigin(0,0)
+
+    this.player = this.add.sprite(40, this.sys.game.config.height/2, 'player')
+    this.player.setScale(0.5)
   }
 }
