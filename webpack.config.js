e7a93353562da91da0ed79ccdcd2f6bcc3557923
mstop4 <mstop4@gmail.com>
2018-04-19 15:58:26 -0400

    updated gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         cache: true,
         parallel: true,
         uglifyOptions: {
-          compress: false,
+          compress: true,
           ecma: 6,
           mangle: true
         },
