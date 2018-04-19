79b14a0466e82f32d289521ccc941b98e450dfba
mstop4 <mstop4@gmail.com>
2018-04-19 12:38:41 -0400

    canvas displayed

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Phaser from 'phaser'
+import { GameScene } from './scenes/gameScene'
+
+const config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 360,
+  parent: 'content',
+  scene: [ GameScene ]
+}
+
+const game = new Phaser.Game(config)
---
+++ src/scenes/gameScene.js
@@ -0,0 +1,9 @@
+import phaser from 'phaser'
+
+export class Preloader extends phaser.Scene {
+  constructor() {
+    super({
+      key: 'gameScene'
+    })
+  }
+}
